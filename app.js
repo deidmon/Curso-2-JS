d3b44546fc424f4734c0d8ece0500e7fb51c4899
deidmon <paul.dedios@alum.udep.edu.pe>
2024-07-07 23:21:59 -0500

    Modulo 2

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Juego del número secreto';
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Elige un número del 1 al 10';
-
-function intentoDeUsuario(){
-    alert('Click desde js')
+let numeroSereto = generarNumeroAleatorio();
+console.log(numeroSereto)
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
 }
+
+function verificarIntento(){
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    if(numeroDeUsuario === numeroSereto){
+        alert('Acertaste')
+    }
+    return;
+}
+
+function generarNumeroAleatorio(){
+    return Math.floor(Math.random()*10)+1;
+}
+
+asignarTextoElemento('h1','Juego del número secreto');
+asignarTextoElemento('p','Elige un número del 1 al 100');
