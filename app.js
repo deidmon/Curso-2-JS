73192888e2acc62ede33d1f5e9675bdd2c6a1d16
deidmon <paul.dedios@alum.udep.edu.pe>
2024-07-14 23:44:44 -0500

    Modulo 3

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-let numeroSereto = generarNumeroAleatorio();
-console.log(numeroSereto)
+let numeroSereto = 0;
+let intentos = 0;
+
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
@@ -9,14 +10,41 @@ function asignarTextoElemento(elemento, texto){
 function verificarIntento(){
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
     if(numeroDeUsuario === numeroSereto){
-        alert('Acertaste')
+       asignarTextoElemento('p',`Acertaste el número en ${intentos} ${intentos === 1 ? 'vez': 'veces'}`);
+       document.getElementById('reiniciar').removeAttribute('disabled');
+    }else {
+        if(numeroDeUsuario > numeroSereto){
+            asignarTextoElemento('p','El número secreto es menor');
+        } else {
+            asignarTextoElemento('p','El número secreto es mayor');
+        }
+        limpiarCaja();
+        intentos ++;
     }
     return;
+}
+
+function condicionesIniciales(){
+    asignarTextoElemento('h1','Juego del número secreto');
+    asignarTextoElemento('p','Elige un número del 1 al 10');
+    numeroSereto = generarNumeroAleatorio();
+    intentos = 1;
+}
+
+function reiniciarJuego(){
+    limpiarCaja();
+    condicionesIniciales();
+    document.querySelector('#reiniciar').setAttribute('disabled', true);
+    
+}
+
+function limpiarCaja(){
+    document.querySelector('#valorUsuario').value = '';
+ 
 }
 
 function generarNumeroAleatorio(){
     return Math.floor(Math.random()*10)+1;
 }
 
-asignarTextoElemento('h1','Juego del número secreto');
-asignarTextoElemento('p','Elige un número del 1 al 100');
+condicionesIniciales();
